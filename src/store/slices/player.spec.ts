8c9e47596b6ffda5b87ba0f619b230da3ab678cb
Guilherme Spati <guilherme.spati723@gmail.com>
2023-08-15 21:06:06 -0300

    finalizado redux

--- src/store/slices/player.spec.ts
+++ src/store/slices/player.spec.ts
@@ -1,27 +1,29 @@
 import { describe, expect, it } from 'vitest';
-import { player as reducer, play, next } from './player';
+import { player as reducer, play, next, PlayerState } from './player';
 
-const exampleState = {
+const exampleState: PlayerState = {
     course: {
+        id: 1,
         modules: [
             {
-                id: '1',
+                id: 1,
                 title: 'Iniciando com React',
                 lessons: [
                     { id: 'Jai8w6K_GnY', title: 'CSS Modules', duration: '13:45' },
                     { id: 'w-DW4DhDfcw', title: 'Estilização do Post', duration: '10:05' },
                 ],
-                },
-                {
-                id: '2',
+            },
+            {
+                id: 2,
                 title: 'Estrutura da aplicação',
                 lessons: [
                     { id: 'gE48FQXRZ_o', title: 'Componente: Comment', duration: '13:45' },
                     { id: 'Ng_Vk4tBl0g', title: 'Responsividade', duration: '10:05' },
                 ],
             },
-          ],
+        ],
     },
+    isLoading: false,
     currentModuleIndex: 0,
     currentLessonIndex: 0,
 }
